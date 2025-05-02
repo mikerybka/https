27f0119aeba9b73c5b508365936b9772a92807f7
Mike Rybka <merybka@gmail.com>
2025-05-02 10:19:29 -0400

    Add StartDev method

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package https
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/mikerybka/util"
@@ -21,4 +22,23 @@ func (s *Server) Start() error {
 	return util.ServeHTTPS(s.Handler, s.Email, s.CertDir, func(host string) bool {
 		return hostAllowlist[host]
 	})
+}
+
+func (s *Server) StartDev(addr string) error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		first, rest, isRoot := util.PopPath(r.URL.Path)
+		if isRoot {
+			w.Header().Set("Content-Type", "text/html")
+			fmt.Fprintf(w, "<pre>\n")
+			for _, h := range s.AllowedHosts {
+				fmt.Fprintf(w, "<a href=\"/%s\">%s\n</a>", h, h)
+			}
+			fmt.Fprintf(w, "</pre>\n")
+			return
+		}
+		r.URL.Path = rest
+		r.Host = first
+		s.Handler.ServeHTTP(w, r)
+	})
+	return http.ListenAndServe(addr, nil)
 }
